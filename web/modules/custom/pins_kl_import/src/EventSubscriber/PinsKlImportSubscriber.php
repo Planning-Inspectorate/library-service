f03ca22b1e6d1b5b2728548062909367cc637e46
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2024-06-12 22:13:38 +0100

    added compound sub docs migration fix

--- web/modules/custom/pins_kl_import/src/EventSubscriber/PinsKlImportSubscriber.php
+++ web/modules/custom/pins_kl_import/src/EventSubscriber/PinsKlImportSubscriber.php
@@ -148,7 +148,7 @@ class PinsKlImportSubscriber implements EventSubscriberInterface {
     switch ($feed_type_id) {
 
       case 'kl_import_metadata':
-      // case 'kl_import_compound_subdocs':
+      case 'kl_import_compound_subdocs':
       // case 'kl_import_compoundfolders':
 
         //entity fields
