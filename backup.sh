f705bbed12790f4043cce5fb6668344c916cce7e
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2024-07-12 18:52:20 +0100

    backup shell command

---
+++ backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Login to MariaDB container
+sudo docker exec -it -u 0 pins_mariadb bash
+
+# Perform MySQL dump
+mysqldump -u root -ppins_9001 kl_drupal > backup_pins-$(date +%Y-%m-%d).sql
+
+# Exit from the container
+exit
+
+# Copy the SQL file to db-backup folder in parent directory
+sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql db-backup/backup_pins-$(date +%Y-%m-%d).sql
+
+# End of script
