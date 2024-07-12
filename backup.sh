398a1b83bbf8826aaada56228cf141d8ffb2f4ee
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2024-07-12 18:54:13 +0100

    backup shell command

--- backup.sh
+++ backup.sh
@@ -9,6 +9,9 @@ mysqldump -u root -ppins_9001 kl_drupal > backup_pins-$(date +%Y-%m-%d).sql
 # Exit from the container
 exit
 
+# Navigate to home folder
+cd /home/KnowledgeDevAdmin/
+
 # Copy the SQL file to db-backup folder in parent directory
 sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql db-backup/backup_pins-$(date +%Y-%m-%d).sql
 
