d62ded9af40d627456e80b249117df96eda0ddb7
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2024-09-09 18:57:40 +0100

    Cron shell command

--- backup-cron.sh
+++ backup-cron.sh
@@ -5,15 +5,15 @@ sudo docker exec -it -u 0 pins_php bash -c "drush watchdog:delete all --yes && d
 sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u root -ppins_9001 kl_drupal > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql"
 
 # Copy the SQL file to db-backup folder in parent directory
-sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql ../db-backup/backup_pins-$(date +%Y-%m-%d).sql
+sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
 
 # Compress the SQL files
-tar -zcvf ../cron-db-backups/backup_pins-$(date +%Y-%m-%d).tar.gz ../db-backup/backup_pins-$(date +%Y-%m-%d).sql
+tar -zcvf /home/KnowledgeDevAdmin/cron-db-backups/backup_pins-$(date +%Y-%m-%d).tar.gz /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
 
 # Remove created db backup from mysql container and db backup folder
 sudo docker exec -it -u 0 pins_mariadb bash -c "rm /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql" 
 
 # Remove created db backup from db backup folder.
-rm ../db-backup/backup_pins-$(date +%Y-%m-%d).sql
+rm /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
 
 # End of script
