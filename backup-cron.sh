a6ff243d5551e9965eada898f5a425dc0025918a
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2024-09-10 13:57:07 +0100

    test messag

--- backup-cron.sh
+++ backup-cron.sh
@@ -1,25 +1,3 @@
-# Login to PHP container
-#sudo docker exec -it -u 0 pins_php bash -c "drush watchdog:delete all --yes && drush cr"
-
-# Login to MariaDB container
-#sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u root -ppins_9001 kl_drupal > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql"
-
-# Copy the SQL file to db-backup folder in parent directory
-#sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
-
-# Compress the SQL files
-#tar -zcvf /home/KnowledgeDevAdmin/cron-db-backups/backup_pins-$(date +%Y-%m-%d).tar.gz /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
-
-# Sleep for 20 seconds
-#sleep 20
-
-# Remove created db backup from mysql container and db backup folder
-#sudo docker exec -it -u 0 pins_mariadb bash -c "rm /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql" 
-
-# Remove created db backup from db backup folder.
-#rm -f /home/KnowledgeDevAdmin/db-backup/backup_pins-$(date +%Y-%m-%d).sql
-
-# End of script
 
 #!/bin/bash
 
@@ -50,3 +28,5 @@ sleep 20
 # Remove the backup files from both the container and the host
 execute_command pins_mariadb "rm $backup_file"
 rm -f "$backup_file"
+
+# End of script
