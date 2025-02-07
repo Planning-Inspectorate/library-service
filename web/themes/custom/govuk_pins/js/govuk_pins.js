d18f7d0c18c6603ab271703ad6ed6ca74c96c3a5
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-02-07 18:28:36 +0000

    test

--- web/themes/custom/govuk_pins/js/govuk_pins.js
+++ web/themes/custom/govuk_pins/js/govuk_pins.js
@@ -29,6 +29,7 @@ function setCookie(cname, cvalue, exdays) {
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires="+ d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  console.log(document.cookie);
 }
 
 function add_blank_target_to_pdf_link() {
