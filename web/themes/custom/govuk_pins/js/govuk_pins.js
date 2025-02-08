e46d80f673db228661377e7e01b86fd107103d76
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-02-08 10:22:40 +0000

    cookie fix

--- web/themes/custom/govuk_pins/js/govuk_pins.js
+++ web/themes/custom/govuk_pins/js/govuk_pins.js
@@ -15,13 +15,11 @@ function toggleMobileMenu() {
 function acceptCookiePolicy () {
   jQuery('.govuk-cookie-banner').slideUp();
   setCookie('cookie_type', 'accept', 90);
-  console.log('acceptCookiePolicy - end');
 }
 
 function rejectCookiePolicy () {
   jQuery('.govuk-cookie-banner').slideUp();
   setCookie('cookie_type', 'reject', 90);
-  console.log('rejectCookiePolicy - end');
 }
 
 function setCookie(cname, cvalue, exdays) {
@@ -30,7 +28,6 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    console.log(document.cookie);
   } catch (error) {
     console.error("Error setting cookie:", error);
   }
