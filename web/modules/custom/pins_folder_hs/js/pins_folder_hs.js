2dd49fb3d1eae617ea445fb01ba185b9e3383f18
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-04-07 09:19:46 +0100

    compound folder changes

--- web/modules/custom/pins_folder_hs/js/pins_folder_hs.js
+++ web/modules/custom/pins_folder_hs/js/pins_folder_hs.js
@@ -1,7 +1,20 @@
 (function ($) {
   Drupal.behaviors.pins_folder_hs = {
     attach: function (context, settings) {
-
+      $(document).ajaxComplete(
+        function (event, xhr, settings) {
+          if (xhr.status === 200) {
+            var response = xhr.responseText;
+            if (response.includes('edit-field-kl-doc-folder-wrapper')) {
+              var tabButtonValue = 2;
+              var tabButton = jQuery('[data-horizontaltabbutton="' + tabButtonValue + '"] a');
+              if (tabButton.length) {
+                tabButton.trigger('click');
+              }
+            }
+          }
+        }
+      );
     }
   } 
 })(jQuery);
