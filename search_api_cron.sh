5c1bc171b6c4f04a398d53c22d6fcaacfe0c7614
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-05-22 14:28:44 +0100

    cron job changes interactive

--- search_api_cron.sh
+++ search_api_cron.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker exec -it -u 0 pins_php bash -c "drush search-api-index --limit=1000 pins_content_index"
+sudo docker exec -u 0 pins_php bash -c "drush search-api-index --limit=1000 pins_content_index"
