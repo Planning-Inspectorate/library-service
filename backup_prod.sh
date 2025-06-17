4ead3829afd0a5d5dc20775926f5c298e935694f
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-06-17 16:50:54 +0100

    altered to zip backup file

--- backup_prod.sh
+++ backup_prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Login to MariaDB container
-sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u d10_pins -ppins_9000 d10_pins_library > /var/lib/mysql/backup_pins-prod-$(date +%Y-%m-%d).sql"
+sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u d10_pins -ppins_9000 d10_pins_library | gzip -c > /var/lib/mysql/backup_pins_prod-$(date +%Y-%m-%d).sql.gz"
 
 # Copy the SQL file to db-backup folder in parent directory
-sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-prod-$(date +%Y-%m-%d).sql ../db-backup/backup_pins-prod-$(date +%Y-%m-%d).sql
+sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins_prod-$(date +%Y-%m-%d).sql.gz ../db-backup/backup_pins_prod-$(date +%Y-%m-%d).sql.gz
 
 # End of script
