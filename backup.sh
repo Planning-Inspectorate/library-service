553f6963f2f2b0ba53fb083a590758ce9ae6ef05
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-06-17 10:27:20 +0100

    cron job with mysqldump compression

--- backup.sh
+++ backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Login to MariaDB container
-sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u root -ppins_9001 kl_drupal > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql"
+sudo docker exec -it -u 0 pins_mariadb bash -c "mysqldump -u root -ppins_9001 kl_drupal | gzip -c > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz"
 
 # Copy the SQL file to db-backup folder in parent directory
-sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql ../db-backup/backup_pins-$(date +%Y-%m-%d).sql
+sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz ../db-backup/backup_pins-$(date +%Y-%m-%d).sql.gz
 
 # End of script
