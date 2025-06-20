567e444a25f7420711729c0b93dfecb19564623e
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-06-20 21:15:21 +0100

    cron with env variable

--- backup-cron.sh
+++ backup-cron.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-echo "Current working directory: $PWD"
-
 if [ -f "$PWD/.env" ]; then # Check if the file named ".env" exists
-    . "$PWD/.env"          # Use the dot command instead of 'source'
+    . "$PWD/.env"
 else
-    echo "Error: Environment file ./.env not found in the current directory!"
+    echo "Error: Environment file not found in the current directory!"
     exit 1
 fi
 
 if [ -z "$DB_NAME" ] || [ -z "$DB_ROOT_PASSWORD" ]; then
-    echo "Error: DB_NAME or DB_ROOT_PASSWORD not set or empty in .env"
+    echo "Error: DB_NAME or DB_ROOT_PASSWORD not set or empty in Environment file"
     exit 1
 fi
+
+PARENT_DIR="$(dirname "$PWD")"
+
+DB_BACKUP_DEST_DIR="$PARENT_DIR/cron-db-backups"
+
 
 # Login to PHP container
 sudo docker exec -u 0 pins_php bash -c "drush watchdog:delete all --yes && drush cr"
@@ -20,9 +23,7 @@ sudo docker exec -u 0 pins_php bash -c "drush watchdog:delete all --yes && drush
 # Login to MariaDB container
 sudo docker exec -u 0 pins_mariadb bash -c "mysqldump -u root -p$DB_ROOT_PASSWORD $DB_NAME | gzip  > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz"
 
-echo "Backup for database '$DB_NAME' completed successfully for environment '$ENV'."
-
 # Copy the SQL file to db-backup folder in parent directory
-sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz /home/KnowledgeDevAdmin/cron-db-backups/backup_pins-$(date +%Y-%m-%d).sql.gz
+sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz $DB_BACKUP_DEST_DIR/backup_pins-$(date +%Y-%m-%d).sql.gz
 
 # End of script
