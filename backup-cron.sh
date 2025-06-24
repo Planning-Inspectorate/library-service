0f3605f4c825ab4413af1d4a4d9794afeb0e48bd
Matthews <sunny.mathews.ta@planninginspectorate.gov.uk>
2025-06-24 01:57:14 +0100

    env file

--- backup-cron.sh
+++ backup-cron.sh
@@ -18,10 +18,6 @@ fi
 
 DB_BACKUP_DEST_DIR="$PWD/cron-db-backups"
 
-# Login to PHP container
-# sudo docker exec -u 0 pins_php bash -c "drush watchdog:delete all --yes && drush cr"
-
-
 # Login to PHP container and clear cache
 echo "Attempting to clear Drush cache and watchdog logs..."
 sudo docker exec -u 0 pins_php bash -c "drush watchdog:delete all --yes && drush cr"
@@ -31,10 +27,22 @@ if [ $? -ne 0 ]; then
 fi
 
 # Login to MariaDB container
+echo "Attempting to dump MariaDB database '$DB_NAME'..."
 sudo docker exec -u 0 pins_mariadb bash -c "mysqldump -u root -p$DB_ROOT_PASSWORD $DB_NAME | gzip  > /var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to dump MariaDB database '$DB_NAME'."
+    exit 1
+fi
+echo "Database '$DB_NAME' dumped successfully inside the container."
+
 
 # Copy the SQL file to db-backup folder in parent directory
+echo "Attempting to copy the SQL file to db-backup folder..."
 sudo docker cp pins_mariadb:/var/lib/mysql/backup_pins-$(date +%Y-%m-%d).sql.gz $DB_BACKUP_DEST_DIR/backup_pins-$(date +%Y-%m-%d).sql.gz
-
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to copy the SQL file to $DB_BACKUP_DEST_DIR."
+    exit 1
+fi
 echo "Copied the SQL file to db-backup folder $DB_BACKUP_DEST_DIR"
+
 # End of script
