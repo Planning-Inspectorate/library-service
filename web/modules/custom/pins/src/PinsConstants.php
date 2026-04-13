dc84f66b37b6a84a2570d8f18ab2f680e56b093d
Michael.Crossman <michael.crossman.d2@planninginspectorate.gov.uk>
2026-04-13 09:47:00 +0100

    Add classifications term ID to constants

--- web/modules/custom/pins/src/PinsConstants.php
+++ web/modules/custom/pins/src/PinsConstants.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 namespace Drupal\pins;
 
 class PinsConstants {
   const START_YEAR = 1800;
   const PINS_REVISION_PATH = 'public://revisions/';
+  const CLASSIFICATIONS_TID = '15952';
 }
